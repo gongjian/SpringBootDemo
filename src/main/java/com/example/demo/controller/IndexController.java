81ce9a112288e7a103fa734745a1c5f8b4376f06
gongjian <gongjian@gongjian-rmbp>
2017-07-11 21:13:16 +0800

    国际化

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,8 @@ public class IndexController {
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("name", "Spring Boot");
+		
+		System.out.println(">>>>>>>>>>>>>>>> Locale: " + LocaleContextHolder.getLocale());
 		
 		return "Hello";
 	}
