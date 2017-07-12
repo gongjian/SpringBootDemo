2dd7f3b87885f894e16208f8924b0001d7526ca0
gongjian <gongjian@gongjian-rmbp>
2017-07-12 20:47:15 +0800

    ssl

--- src/main/java/com/example/demo/configuration/WebConfiguration.java
+++ src/main/java/com/example/demo/configuration/WebConfiguration.java
@@ -1,15 +1,10 @@
 package com.example.demo.configuration;
 
-import java.io.File;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -109,6 +104,5 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
 		registry.addResourceHandler("/internal/**").addResourceLocations("classpath:/");
 	}
-
 
 }
